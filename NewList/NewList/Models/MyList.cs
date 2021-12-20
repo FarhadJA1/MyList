4798bcd7326310edd2a79955e539c49215424a2d
Farhad Azadaliyev <farhadja@code.edu.az>
2021-12-20 07:37:43 -0800

    initial

---
+++ NewList/NewList/Models/MyList.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NewList.Models
+{
+    public class MyList<T> 
+    {
+        
+        public T[] MyArray;
+        public int Count { get; set; }
+
+        public MyList()
+        {
+            MyArray = new T[0];
+        }
+
+        public void Add(T obj)
+        {
+            if (Count % 10 == 1)
+            {
+                Array.Resize(ref MyArray,MyArray.Length + 10);
+            }
+            
+            obj =  MyArray[Count];
+            Count++;
+        }
+
+        public void Delete()
+        {
+            MyArray = new T[Count];
+            Count = 0;
+        }
+
+        public void AddRange(IEnumerable<T> List)
+        {
+            foreach (var item in List)
+            {
+                Add(item);
+            }
+        }
+
+        public T Find(Predicate<T> func)
+        {
+            foreach (var item in MyArray)
+            {
+                if(func(item))
+                {
+                    return item;
+                }
+
+            }
+            return default;
+        }
+
+        public void GetAll()
+        {
+            foreach (var item in MyArray)
+            {
+                Console.WriteLine(item); 
+            }
+            
+        }
+
+        
+    }
+}
---
+++ NewList/NewList/Program.cs
@@ -0,0 +1,16 @@
+﻿using NewList.Models;
+using System;
+
+namespace NewList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList<string> myList = new MyList<string>();
+            myList.Add("Farhad");
+            myList.Add("Tural");
+            
+      }
+    }
+}
